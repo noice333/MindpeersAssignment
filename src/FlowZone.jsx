f555679778ac4b5cd72fe16f90ce8c181ce397c0
Sri Harsha Polamarasetty <55107951+noice333@users.noreply.github.com>
2023-11-09 00:29:55 +0530

    Update FlowZone.jsx

--- src/FlowZone.jsx
+++ src/FlowZone.jsx
@@ -39,7 +39,7 @@ const FlowZone = ({ children }) => {
     ]
     return (
         <div className="flow-zone" style={{ color: theme.text }}>
-            <div className="row m-0 p-0">
+            <div className="row m-0 px-0 pb-5">
                 <div className="col-sm-12 col-md-6 mx-auto border border-4 border-secondary rounded-4">
                     <div className="h5 py-4 text-center">Flow Zone</div>
                     <div className="d-flex justify-content-between align-items-center px-4 py-2" style={{ borderRadius: '10px', background: theme.cardBackground }}>
